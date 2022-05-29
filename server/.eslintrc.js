53b1926c192e688e7dcdee92eca2a0ddd1260aab
Liangjunting <liangjunting@bignox.com>
2022-05-29 23:57:20 +0800

    fix: eslint规则

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
 	],
 	'rules': {
 		'no-var': 0,
+		'no-trailing-spaces': 'error',  // 禁止行尾空格
+    'no-multiple-empty-lines': [2, { 'max': 1 }],   // 最大空行1行
 		'@typescript-eslint/no-var-requires': 0,
 		'quotes': ['error', 'single'],
 		'semi': ['error', 'always']
